c4a9b888e2915a939a43547b4ccad9b1d7af06ee
Joshua P Johnson <joshpauljohnson@gmail.com>
2013-01-02 19:22:11 -0500

    No need to invoke userDetailsService, just need to return userIds from connection service

--- src/main/java/com/solairis/dayssince/connect/UserConnectionSignUp.java
+++ src/main/java/com/solairis/dayssince/connect/UserConnectionSignUp.java
@@ -6,12 +6,9 @@ package com.solairis.dayssince.connect;
 
 import java.util.List;
 import javax.annotation.Resource;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionSignUp;
 import org.springframework.social.connect.mongo.ConnectionService;
@@ -26,8 +23,6 @@ public class UserConnectionSignUp implements ConnectionSignUp {
 
 	@Resource
 	private ConnectionService connectionService;
-	@Resource
-	private UserDetailsService namePassUserService;
 
 	@Override
 	public String execute(Connection<?> connection) {
@@ -41,8 +36,7 @@ public class UserConnectionSignUp implements ConnectionSignUp {
 				// Create new user
 				return connection.getDisplayName();
 			} else if (userIds.size() == 1) {
-				UserDetails userDetails = this.namePassUserService.loadUserByUsername(userIds.get(0));
-				return userDetails.getUsername();
+				return userIds.get(0);
 			} else {
 				throw new IllegalArgumentException("Only 1 user Id may be associated with a provider. We found "+userIds.size());
 			}
